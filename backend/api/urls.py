fc57401c0123954370af31b9c10111c57cd64866
Vadim Veshnyakov <veshvp00@gmail.com>
2025-01-23 21:19:27 +0300

    redoing3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,6 +15,6 @@ router.register(r'users', UserViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
-    path('<str:short_id>/', recipe_redirect,
+    path('s/<str:short_id>/', recipe_redirect,
          name='recipe_redirect'),
 ]
